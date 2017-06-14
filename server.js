ebfdb1a73339fa41bd7f5d98bbfb288f51ab9997
cestilloso <shessa.estilloso@gmail.com>
2017-06-14 14:52:24 +0800

    2nd commit

--- server.js
+++ server.js
@@ -11,3 +11,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.get('/', function(req, res){
   res.sendFile(path.join(__dirname, 'views/assign.html'));
 });
+
+//express server listen
+var server = app.listen(app.get('port'), function(){
+  console.log('Server listening on port ',app.get('port'));
+});

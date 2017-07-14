efa177bffcbaf33a4e159a4ddf31a52ea76c7e55
cestilloso <shessa.estilloso@gmail.com>
2017-07-14 19:16:15 +0800

    first

--- server.js
+++ server.js
@@ -9,7 +9,10 @@ app.set('port', (process.env.PORT || 5000));
 app.use(express.static(path.join(__dirname, 'public')));
 //express routes
 app.get('/', function(req, res){
-  res.sendFile(path.join(__dirname, 'views/assign.html'));
+  res.sendFile(path.join(__dirname, 'views/index.html'));
+});
+app.get('/views', function(req, res){
+  res.sendFile(path.join(__dirname, 'views/sub.html'));
 });
 
 //express server listen
